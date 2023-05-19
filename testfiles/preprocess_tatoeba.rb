5828c7b2a776218f53737789fb7619e43531f15a
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2023-05-19 18:54:41 +0300

    Some scripts

---
+++ testfiles/preprocess_tatoeba.rb
@@ -0,0 +1,17 @@
+def interesting(str)
+  total = 0
+  ascii = 0
+  str.unpack("U*").each do |ch|
+    ascii += 1 unless ch.ord >= 0x80
+    total += 1
+  end
+  return ascii.to_f / total < 0.90
+end
+
+out = File.open("tatoeba-nonascii-CC0.csv", "wb")
+
+File.open("sentences_CC0.csv").each_line do |l|
+  tmp = l.strip.split("\t")
+  next unless interesting(tmp[2])
+  out.printf("%s\t%s\t%s\n", tmp[0], tmp[1], tmp[2])
+end
---
+++ testfiles/utf8freqstats.rb
@@ -0,0 +1,17 @@
+# read UTF-8 text from stdout and analyze its properties
+
+freqblk = {}
+freqch = {}
+
+STDIN.read.unpack("U*").each do |ch|
+  uniblock = ch.ord >> 7
+  freqblk[uniblock] = (freqblk[uniblock] || 0) + 1
+  freqch[ch.ord] = (freqch[ch.ord] || 0) + 1
+end
+
+freqblk.to_a.sort {|a, b| b[1] <=> a[1] }.take(10).each do |x|
+  uniqcnt = 0
+  (x[0] << 7).upto((x[0] << 7) + 0x7F).each {|y| uniqcnt += 1 if freqch.has_key?(y) }
+  printf("%04X..%04X range: %10d hits (%d out of 128 actually used)\n",
+         x[0] << 7, (x[0] << 7) + 0x7F, x[1], uniqcnt)
+end
