127cb38a222a85aed9b9bc028595e4d1a5bfbe46
Siarhiej Siamaška <siarhei.siamashka@gmail.com>
2026-08-30 05:07:24 +0300

    training for an arm binary

---
+++ get_english_texts.sh
@@ -0,0 +1,7 @@
+wget https://www.gutenberg.org/cache/epub/2701/pg2701.txt
+wget https://www.gutenberg.org/cache/epub/11/pg11.txt
+wget https://www.gutenberg.org/cache/epub/45839/pg45839.txt
+wget http://mattmahoney.net/dc/enwik8.zip
+unzip enwik8.zip
+rm enwik8.zip
+mv enwik8 enwik8.txt
